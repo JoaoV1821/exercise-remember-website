42ddfbc4208ef3a74e1259261cde6939873c7545
BrunaRavelli-byte <brunaravelli13@gmail.com>
2022-04-28 23:04:37 -0300

    Acrescentei alguns eventos, mudifiquei algumas mensagens e excluí alguns links

--- js/recSenha.js
+++ js/recSenha.js
@@ -1,4 +1,13 @@
+$('#form').click(function(event){
+    event.preventDefault()
+    })
+
+$('#btn').click(function(event){
+    event.preventDefault()
+    })
+
 function validacaoEmail(field) {
+    document.getElementById('btn').style.color = 'white';
     usuario = field.value.substring(0, field.value.indexOf("@"));
     dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
     com = field.value.substring(field.value.indexOf("com"));
@@ -13,11 +22,11 @@ function validacaoEmail(field) {
         (dominio.indexOf(".") >=1)&&
         (com.lastIndexOf('com')!= -1)&&
         (dominio.lastIndexOf(".") < dominio.length - 1))  {
-    $("msgemail").innerHTML="E-mail válido";
+    document.getElementById('btn').innerText = 'Solicitação enviada, verifique seu email!';
     alert('Recuperação de senha efetuado com sucesso, verifique seu email!')
     }else{
-    $("msgemail").innerHTML="<font color='red'>E-mail inválido </font>";
-    alert("[E-mail invalido], favor informar um email válido!");
+    alert('[Atenção], o email digitado não corresponde a um email válido, favor digite o email cadastrado!')
+    document.getElementById('btn').style.color = 'red';
     }
     }
 
